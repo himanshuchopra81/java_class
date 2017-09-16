7490e93681fa1047dee245079ac63fd914c6a336
Himanshu Chopra <691062@campus.loc>
2017-09-16 19:35:53 -0400

    used if statement to check grade

---
+++ week02/GradeCheckIf.java
@@ -0,0 +1,37 @@
+import java.lang.invoke.SwitchPoint;
+import java.util.Scanner;
+
+class GradeCheckIf  {
+
+	public static void main(String args[]) {
+		GradeCheckIf f = new GradeCheckIf();
+		Scanner gradescan = new Scanner(System.in);	
+		System.out.println("Enter grade");
+		int gradeVal = gradescan.nextInt();
+
+		System.out.println(f.calculateGrade(gradeVal));
+		
+		
+	}
+	public String calculateGrade(int grade) {
+		if(grade >= 90) {
+			return "A";
+		}
+		if(grade >= 80) {
+			return "B";
+		}
+		if(grade >= 70) {
+			return "C";
+		}
+		if(grade >= 60) {
+			return "D";
+		}
+		else {
+			return "E";
+		}
+			
+		
+	}
+
+}
+
